2c8b86a8eb95777bc9c6b0f97994713ad5f218a7
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-01-14 11:09:20 -0700

    Finish whole game

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const arrOne = ["👊🏽", "🖐🏽", "✌🏽"];
 const arrTwo = ["✌🏽", "👊🏽", "🖐🏽"];
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function getComputerChoice() {
@@ -24,16 +24,9 @@ function playOneRound(computerChoice, playerChoice) {
 
   return roundWinner;
 }
-async function displayComputerChoice(choice){
+function displayComputerChoice(choice) {
   const computerChoiceBox = document.querySelector(".computer-choice-icon");
-  for (let i = 0; i < 3; i++) {
-    computerChoiceBox.textContent = arrOne[i];
-    await sleep(250);
-  }
-
-  computerChoiceBox.textContent = choice;
-
-  console.log('this will fire after all delays');
+  computerChoiceBox.innerText = choice;
 }
 
 function updateUI(roundWinner, currentRound) {
@@ -52,23 +45,50 @@ function updateUI(roundWinner, currentRound) {
       roundIcons[currentRound + 1].classList.add("current-round");
       break;
   }
+}
 
-  // Play animation for overall winner
+function winnerAnimation(winner) {
+  const roundIcons = document.querySelectorAll(".round-icon");
+  const roundBoxText = document.querySelector(".game-status h2");
+  const gameScreen = document.querySelector(".game-screen");
+
+  gameScreen.removeChild(document.querySelector(".player-choice"));
+  gameScreen.removeChild(document.querySelector(".computer-choice"));
+
+  let winnerClass = winner === "player" ? "player-won" : "computer-won";
+
+  roundBoxText.innerText = `Winner: ${winner}`;
+  roundIcons.forEach((roundIcon) => {
+    roundIcon.classList.add(winnerClass);
+    roundIcon.classList.remove("current-round");
+  });
 }
 
 let playerChoice = null;
 let computerChoice = null;
 let currentRound = 0;
 
+let playerWinCount = 0;
+let computerWinCount = 0;
+let roundWinner;
 const playerChoiceIcon = document.querySelectorAll(".player-choice-icon");
 playerChoiceIcon.forEach((currentElement) => {
   currentElement.addEventListener("click", (e) => {
     playerChoice = e.target.innerText;
     computerChoice = getComputerChoice();
-    roundWinner = playOneRound(computerChoice, playerChoice);
+    roundWinner = playOneRound(computerChoic  e, playerChoice);
 
-    playComputerChoiceAnim(computerChoice);
+    if(roundWinner === 'player') playerWinCount += 1;
+    else if(roundWinner === 'computer') computerWinCount += 1;
+    
+    displayComputerChoice(computerChoice);
     updateUI(roundWinner, currentRound);
-    if (roundWinner !== "tie") currentRound++;
+
+    if (roundWinner !== "tie") {
+      currentRound++;
+    }
+
+    if (playerWinCount === 3) winnerAnimation('player');
+    if (computerWinCount === 3) winnerAnimation('computer');
   });
 });
